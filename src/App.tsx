2e5895235e7e0d67752f6260d4bbfec1d6372d81
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-02-08 22:25:15 +0000

    Chakra test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { Button } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello NutraCount!</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </>
   )
 }
 
